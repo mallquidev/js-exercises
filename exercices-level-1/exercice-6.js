56db4533bfbfe945fc74edfc18ee3a2d77aa6299
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-06-20 14:56:57 -0500

    exercise 6 resolved

--- exercices-level-1/exercice-6.js
+++ exercices-level-1/exercice-6.js
@@ -1,2 +1,26 @@
 /*Añadir un nuevo botón al ejercicio 
 anterior, que al pulsarlo cambie la imagen. */
+
+const button2 = document.getElementById('button2')
+const caja = document.getElementById('caja')
+let perroxd = './perroxd.jpeg'
+
+button2.addEventListener('click', addImagen)
+
+let estadoNew = true
+function addImagen() {
+    console.log(estadoNew)
+    if(estadoNew){
+        const img = document.getElementById('imagen')
+        img.src = perroxd
+        estadoNew = false    
+    } else {
+        const img = document.getElementById('imagen')
+        if (img) {
+            caja.removeChild(img)
+            console.log(img)
+        }
+    }
+    
+    
+}
