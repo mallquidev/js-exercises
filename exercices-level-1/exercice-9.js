4f24affd275be0e969aa41ee13ef6ad80cf38601
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-06-24 13:25:41 -0500

    exercice 9 solved

--- exercices-level-1/exercice-9.js
+++ exercices-level-1/exercice-9.js
@@ -6,7 +6,32 @@ formulario.
 El contenedor donde se muestra el nombre, tendrá 
 además un botón para regresar al formulario.*/
 
+const textDiv = document.getElementById('saludo')
+const divForm = document.getElementById('divForm')
 const buttons = document.querySelectorAll('button')
 const button = buttons[0]
 
-button.addEventListener('click', )
+button.addEventListener('click', mostrarTitulo)
+
+function mostrarTitulo(e){
+    const input = document.getElementById('nombre')
+    const texto = input.value
+    console.log(texto)
+    const h1 = document.createElement('h1')
+    h1.innerText = input.value
+    textDiv.appendChild(h1)
+    divForm.style.display = 'none'
+    
+    const buttonOn = document.createElement('button')
+    buttonOn.id = 'btnOn'
+    buttonOn.textContent = 'Volver'
+    textDiv.appendChild(buttonOn);
+    
+    buttonOn.addEventListener('click', function() {
+        divForm.style.display = 'block'
+    })
+
+    e.preventDefault()
+    
+}
+
