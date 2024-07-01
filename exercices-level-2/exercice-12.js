fb224a4d40a7dcc96ce2cdad86278476d67b6b6d
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-07-01 11:11:59 -0500

    exercice 12 solved

--- exercices-level-2/exercice-12.js
+++ exercices-level-2/exercice-12.js
@@ -6,3 +6,24 @@
  * - NO hace falta comprobar que ambas palabras existan.
  * - Dos palabras exactamente iguales no son anagrama.
  */
+
+function verifyAnagrama(val1, val2) {
+    let prompt1 = val1.toLowerCase(); //odin
+    let prompt2 = val2.toLowerCase(); //nido
+    
+    let valor1 = prompt1.split('').sort().join('')
+    let valor2 = prompt2.split('').sort().join('')
+
+    if(valor1 === valor2) {
+        return true
+    } else {
+        return false
+    }
+    
+    
+    
+
+}
+let palabra1 = prompt('Ingrese una palabra')
+let palabra2 = prompt('ingrese el anagrama')
+console.log(verifyAnagrama(palabra1, palabra2))
