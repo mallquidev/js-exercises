964eed725b2f8ca8ecdd8e8f65b24e8e111a2161
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-07-03 15:02:32 -0500

    exercice14 solved

--- exercices-level-2/exercice-14.js
+++ exercices-level-2/exercice-14.js
@@ -2,3 +2,21 @@
  * Escribe un programa que se encargue de comprobar si un número es o no primo.
  * Hecho esto, imprime los números primos entre 1 y 100.
  */
+
+function primo(num){
+    if(num <= 1) return false;
+    for(let i = 2; i < num; i++){
+        if(num % i === 0) return false;
+    }
+    return true
+}
+
+function imprimirPrimos(){
+    for(let i = 1; i <= 100; i++){
+        if(primo(i)){
+            console.log(i)
+        }
+    }
+}
+
+imprimirPrimos()
